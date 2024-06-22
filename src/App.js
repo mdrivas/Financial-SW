1ac2d74dbcbbf8f28ea07bec053f3bbca4175cbf
Mattheos Drivas <110428845+mdrivas@users.noreply.github.com>
2024-06-22 12:25:41 -0700

    Updating Homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [clientsList, setClientsList] = useState(null);
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:5000/api/data')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setClientsList(data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the data!', error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React and Flask</h1>
+      {clientsList ? (
+        clientsList.map(client => (
+          <div key={client.id}>
+            <p>{client.first_name} {client.last_name} ({client.email})</p>
+          </div>
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
