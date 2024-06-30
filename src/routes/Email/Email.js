7f5d354e6b6be2b2022a9114715c8dab721644d6
Alexandros Drivas <alexandrosdrivas@Alexandross-MacBook-Pro.local>
2024-06-29 19:46:21 -0700

    Add send email button

--- src/routes/Email/Email.js
+++ src/routes/Email/Email.js
@@ -2,10 +2,29 @@
 import React from 'react';
 
 function Email() {
+  // Function to send an email
+  const sendEmail = () => {
+    console.log("Sending Email...");
+    fetch("http://127.0.0.1:5000/send_email", {
+      method: "POST" 
+    })
+    .then(response => {
+      if (response.ok) {
+        console.log("Email sent Successfully");
+      } else {
+        console.error("Failed to send email");
+      }
+    })
+    .catch(error => {
+      console.error("Error:", error);
+    });
+  };
+
   return (
     <div>
       <h1>Email Page</h1>
       <p>This is a simple email page with some information.</p>
+      <button type="button" onClick={sendEmail}>Send Email</button>
     </div>
   );
 }
